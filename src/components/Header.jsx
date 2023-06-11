9be585ffea4f8ec39711ccd46792513ec93cdb11
Srivatsav <srivatsav.vijay2003@gmail.com>
2023-06-11 20:10:21 +0530

    commit

---
+++ src/components/Header.jsx
@@ -0,0 +1,14 @@
+import React from "react"
+
+export default function Header() {
+    return (
+        <header className="header">
+            <img 
+                src="troll-face.png" 
+                className="header--image"
+            />
+            <h2 className="header--title">Meme Generator</h2>
+            <h4 className="header--project">Srivatsav</h4>
+        </header>
+    )
+}
